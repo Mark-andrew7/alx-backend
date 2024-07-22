c253bcfd72c636c8cdb476f0857f3d234a32e956
Mark-andrew7 <kamaumark19@gmail.com>
2024-07-22 19:52:10 +0300

    Added newline at end of the line

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -14,6 +14,9 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """
+        Initialiazer
+        """
         self.__dataset = None
         self.__indexed_dataset = None
 
