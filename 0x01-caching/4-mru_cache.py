7f0dbc688d8cf957e4c3fb784590558045275353
Mark-andrew7 <kamaumark19@gmail.com>
2024-07-24 11:17:17 +0300

    MRU caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+class MRUCache that inherits from BaseCaching and is a caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache caching system that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initializer
+        """
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.stack.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last = self.stack.pop()
+                del self.cache_data[last]
+                print("DISCARD: {}".format(last))
+            self.cache_data[key] = item
+            self.stack.append(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.stack.remove(key)
+        self.stack.append(key)
+        return self.cache_data[key]
